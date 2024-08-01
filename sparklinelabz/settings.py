68c7f5a37ccf7c61183b69a0fd834ceae2e59ad2
Muhammad Bachal <mbachalabro1@gmail.com>
2024-08-01 23:22:30 +0500

    Statics Served Locally

--- sparklinelabz/settings.py
+++ sparklinelabz/settings.py
@@ -132,18 +132,13 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 AWS_S3_FILE_OVERWRITE=False
 
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage"
-    },
-    "staticfiles": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage"
-    }, 
+# S3 storage configuration for media files
 
-}
-
+# Media files configuration
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
