7913aee2d51c97c5b5b2a6602fd04d6b1adab38b
Muhammad Bachal <mbachalabro1@gmail.com>
2024-08-01 23:39:58 +0500

    Static Config changed

--- sparklinelabz/settings.py
+++ sparklinelabz/settings.py
@@ -119,9 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # AWS S3 Setup
 
 AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID")
